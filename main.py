5b113dcc30610a549c93c3a3166f78b97afc529b
Eitan Cohen <44434461+eitancohen1@users.noreply.github.com>
2022-08-03 14:56:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import os
 
 def findLine():
-    f = open("enterSandman.txt", "r")
-    n=input("Enter a line: ")
-    if n.isnumeric():
-        n=int(n)
-        for i in range(0,n-1):
-            f.readline()
-        print(f.readline())
-    else:
-        os.system('cls')
-        print("wrong, try again")
+    a=input("enter a song: ")
+    b=".txt"
+    song=a+b
+    try:
+        f = open(song, "r")
+        n=input("Enter a line: ")
+        if n.isnumeric():
+            n=int(n)
+            for i in range(0,n-1):
+                f.readline()
+            print(f.readline())
+        else:
+            os.system('cls')
+            print("wrong input, try again")
+            findLine()
+    except:
+        print("not defined, try again")
         findLine()
 
-def restart():
+
+def restart():  
     start=input("do you wont to search another line? y/n: ")
     if start=="y":
         os.system('cls')
@@ -34,3 +42,16 @@ def main(start):
 
 os.system('cls')
 main("y")
+
+
+
+
+
+
+'''
+f = open("enterSandman.txt", "r")
+    n=int(input("Enter a line: "))
+    for i in range(0,n-1):
+        f.readline()
+    print(f.readline())
+'''
